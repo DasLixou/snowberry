86534cc722da23d164dd906f61a4f037f62af354
DasLixou <einlixou@gmail.com>
2024-05-05 11:48:48 +0200

    we can drop scopes again :D

--- crates/snowberry_core/src/scope.rs
+++ crates/snowberry_core/src/scope.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::{cell::RefCell, marker::PhantomData};
 
 use bumpalo::Bump;
 use slotmap::new_key_type;
@@ -11,23 +11,60 @@ new_key_type! {
 pub struct ScopeLife<'scope>(pub PhantomData<&'scope ()>);
 
 pub struct Scope {
-    store: Bump,
+    pub store: ScopeStore,
     pub sub_scopes: Vec<ScopeKey>,
 }
 
 impl Scope {
     pub fn new() -> Self {
         Self {
-            store: Bump::new(),
-            sub_scopes: vec![],
+            store: ScopeStore {
+                store: Bump::new(),
+                drops: RefCell::new(Vec::new()),
+            },
+            sub_scopes: Vec::new(),
         }
     }
+}
 
+pub struct ScopeStore {
+    store: Bump,
+    drops: RefCell<Vec<StoreDropper>>,
+}
+
+impl ScopeStore {
     pub fn store<'scope, T: 'scope>(&self, _life: ScopeLife<'scope>, val: T) -> &'scope T {
         let b = self.store.alloc(val);
+        if std::mem::needs_drop::<T>() {
+            self.drops.borrow_mut().push(StoreDropper::new(b as *mut T));
+        }
         unsafe {
             // TODO: think about safety
             core::mem::transmute(b)
         }
     }
+}
+
+struct StoreDropper {
+    pointer: *mut (),
+    drop_fn: fn(*mut ()),
+}
+
+impl StoreDropper {
+    pub fn new<T>(pointer: *mut T) -> Self {
+        StoreDropper {
+            pointer: pointer as *mut (),
+            drop_fn: erased_drop::<T>,
+        }
+    }
+}
+
+impl Drop for StoreDropper {
+    fn drop(&mut self) {
+        (self.drop_fn)(self.pointer)
+    }
+}
+
+fn erased_drop<T>(erased_ptr: *mut ()) {
+    unsafe { core::ptr::drop_in_place(erased_ptr.cast::<T>()) }
 }
