94ced2e28a7399762ac90be6bd8a5da86eb25d58
DasLixou <einlixou@gmail.com>
2025-03-10 19:24:09 +0100

    start playing around with nested composables

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,4 +1,4 @@
-use snowberry::{composable, composable::Composable};
+use snowberry::{composable, composable::Composable, composition::Composition};
 
 fn main() {
     let bomb = Bomb("static");
@@ -10,6 +10,13 @@ fn simple<'l>() -> impl Composable<'l> {
     composable!(|cx| {
         let cx = cx.store(Bomb("42"));
         let cx = cx.store(Bomb("another one :3"));
+        let cx = cx.store(Composition::open(window()));
+        cx
+    })
+}
+
+fn window<'l>() -> impl Composable<'l> {
+    composable!(|cx| {
         let cx = cx.store(Bomb("..and the window second :>"));
         let cx = cx.store(Bomb("Uninitialize render cx first.."));
         cx
