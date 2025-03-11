82319bbd02e09998abae28b8b195d4005eff305a
DasLixou <einlixou@gmail.com>
2025-03-11 20:27:04 +0100

    cleanup window

--- snowberry_winit/src/window.rs
+++ snowberry_winit/src/window.rs
@@ -1,6 +1,4 @@
-use snowberry::{
-    composable, composable_::Composable, composition::Composition, lens_table::LensTable,
-};
+use snowberry::{composable, composable_::Composable, lens_table::LensTable};
 use winit::window::WindowAttributes;
 
 use crate::runner::LOOP;
@@ -14,8 +12,7 @@ pub fn window<'life, D: Copy + 'life>(
         let window = LOOP.get().unwrap().create_window(attributes).unwrap();
         let cx = cx.store(window);
         // TODO: provide access to window down the tree
-        let (cx, comp) = Composition::open(cx, child);
-        let cx = cx.store(comp);
+        let cx = cx.compose(child);
         cx
     })
 }
