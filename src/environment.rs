c9a59d8de699fa59a1bc09f821959e82eff75d25
DasLixou <einlixou@gmail.com>
2025-03-15 16:50:01 +0100

    some env progress

--- src/environment.rs
+++ src/environment.rs
@@ -1,17 +1,50 @@
-// TODO: find a solution to get it working without specialization
+use std::convert::Infallible;
 
-// trait WithEnv<T> {
-//     fn get(&self) -> &T;
-// }
+trait Envy {}
+impl Envy for i32 {}
 
-// impl<Other, T> WithEnv<T> for (Other, T) {
-//     fn get(&self) -> &T {
-//         &self.1
-//     }
-// }
+trait MayHave<T> {
+    /// Either `Infallible` or `T`
+    type Res;
+}
 
-// impl<Down: WithEnv<T>, T, Other> WithEnv<T> for (Down, Other) {
-//     fn get(&self) -> &T {
-//         self.0.get()
-//     }
-// }
+impl<T> MayHave<T> for () {
+    type Res = Infallible;
+}
+
+trait Find<T, Down: MayHave<T>, Res = <Down as MayHave<T>>::Res> {
+    type Output;
+}
+
+trait WithEnv<T> {}
+
+impl<T: Envy, Down> Find<T, Down, Infallible> for (Down, T)
+where
+    Down: MayHave<T>,
+{
+    type Output = T;
+}
+
+impl<T: Envy, Down, Cur> Find<T, Down, T> for (Down, Cur)
+where
+    Down: MayHave<T>,
+{
+    type Output = Down::Res;
+}
+
+impl<T, Down, Cur> MayHave<T> for (Down, Cur)
+where
+    Down: MayHave<T>,
+    Self: Find<T, Down>,
+{
+    type Res = <Self as Find<T, Down>>::Output;
+}
+
+impl<T, M> WithEnv<T> for M where M: MayHave<T, Res = T> {}
+
+fn test<Env: WithEnv<i32>>(_env: Env) {
+    test(((), 32i32));
+    test((((), 32i32), 32i32));
+    test((((), 32i32), 32i16));
+    // test((((), 32i8), 32i16));
+}
